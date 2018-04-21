8405b17396e0d17f915f83939c770a01d82d258c
matthieu <matthieu.cneude@gmail.com>
2018-04-21 16:13:32 +0200

    [master] It's a mess... but it works :D

--- main.go
+++ main.go
@@ -4,12 +4,14 @@ import (
 	"bytes"
 	"flag"
 	"fmt"
-	"github.com/Phantas0s/watcher"
-	"github.com/kylelemons/go-gypsy/yaml"
 	"log"
 	"os"
 	"os/exec"
+	"strings"
 	"time"
+
+	"github.com/Phantas0s/watcher"
+	"github.com/kylelemons/go-gypsy/yaml"
 )
 
 func main() {
@@ -21,13 +23,14 @@ func main() {
 	// 2 since the event can be writting file or writting directory ... to fix
 	w.SetMaxEvents(2)
 	w.FilterOps(watcher.Write)
+	filePath := extractScalar(config, "watcher.folder")
 
 	go func() {
 		for {
 			select {
 			case event := <-w.Event:
 				if !event.IsDir() {
-					result := fireCmd(config, event)
+					result := fireCmd(config, event, filePath)
 					fmt.Println(result)
 				}
 			case err := <-w.Error:
@@ -38,7 +41,6 @@ func main() {
 		}
 	}()
 
-	filePath := extractScalar(config, "watcher.folder")
 	root := toYamlMap(config.Root)
 	ext := extractExt(root)
 	if err := w.AddSpecificFiles(filePath, ext); err != nil {
@@ -51,30 +53,39 @@ func main() {
 	}
 }
 
-func fireCmd(config *yaml.File, event watcher.Event) string {
+func fireCmd(config *yaml.File, event watcher.Event, filepath string) string {
 	cmdPath, err := config.Get("watcher.command_path")
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	fmt.Println(event.Name())
-	result := execCmd(cmdPath, event.Path)
+	options := extractOpt(toYamlMap(config.Root))
+
+	path := event.Path
+	if isAbsolute(event.Path) {
+		path = getRelative(event.Path, filepath)
+	}
+
+	options = append(options, path)
+	result := execCmd(cmdPath, options)
 
 	return result
 }
 
-func execCmd(cmdPath string, args ...string) string {
-	clear := exec.Command("clear")
-	cmd := exec.Command(cmdPath, args...)
-
+func execCmd(cmdPath string, args []string) string {
 	var out bytes.Buffer
+	var stderr bytes.Buffer
+	cmd := exec.Command(cmdPath, args...)
+	cmd.Stderr = &stderr
 	cmd.Stdout = &out
-	clear.Stdout = os.Stdout
 
+	clear := exec.Command("clear")
+	clear.Stdout = os.Stdout
 	clear.Run()
+
 	err := cmd.Run()
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println(fmt.Sprint(err) + ": " + stderr.String())
 	}
 
 	return out.String()
@@ -115,6 +126,19 @@ func extractExt(root yaml.Map) []string {
 	return result
 }
 
+func extractOpt(root yaml.Map) []string {
+	list := toYamlList(toYamlMap(root["watcher"])["command_options"])
+	result := make([]string, list.Len())
+
+	for k, v := range list {
+		str := toYamlScalar(v).String()
+		str = strings.Replace(str, "'", "", -1)
+		result[k] = str
+	}
+
+	return result
+}
+
 func extractScalar(config *yaml.File, name string) string {
 	entry, err := config.Get(name)
 	if err != nil {
@@ -122,4 +146,19 @@ func extractScalar(config *yaml.File, name string) string {
 	}
 
 	return entry
+}
+
+func isAbsolute(path string) bool {
+	if path[0] == '/' {
+		return true
+	}
+
+	return false
+}
+
+func getRelative(path string, filepath string) string {
+	newpath := strings.SplitAfter(path, filepath)
+	path = filepath + newpath[1]
+
+	return path
 }
